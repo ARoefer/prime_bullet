27622da523a0d577084c17082c663ef7d9bf735b
Adrian Röfer <aroefer@uni-bremen.de>
2019-06-16 22:23:39 +0200

    Added short script for loading and displaying URDFs in bullet.

---
+++ scripts/view_urdf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+from time import time
+from math import sin
+
+from iai_bullet_sim.basic_simulator import BasicSimulator
+
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) < 2:
+            print('Name of urdf to load required. ROS package URIs are supported.')
+    else:
+        
+        sim = BasicSimulator()
+        sim.init(mode='gui')
+        floor    = sim.create_box(extents=[10,10,0.1], mass=0)
+        thing = sim.load_urdf(sys.argv[1], useFixedBase=1)
+
+        last_update = time()
+        while True:
+            if time() - last_update >= sim.time_step:
+                sim.update()
+                last_update = time()
