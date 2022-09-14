844ab891d23c26ecdf7f69b43f9b9303747d8859
Adrian Röfer <aroefer@uni-bremen.de>
2022-09-14 16:59:31 +0200

    Added top-level imports

--- src/iai_bullet_sim/__init__.py
+++ src/iai_bullet_sim/__init__.py
@@ -1,3 +1,31 @@
 from pathlib import Path
 
 IAI_BULLET_ROOT = Path(__file__).parent
+
+from .geometry      import Point3, \
+                           Vector3, \
+                           Quaternion, \
+                           Transform, \
+                           AABB
+
+from .frame         import Frame
+from .camera        import Camera, PerspectiveCamera
+from .laser_scanner import LaserScanner
+from .rigid_body    import RigidBody, \
+                           BoxBody, \
+                           CylinderBody, \
+                           SphereBody, \
+                           MeshBody, \
+                           SDFBody, \
+                           SDFWorldBody
+
+from .multibody     import MultiBody, Link
+from .constraint    import Constraint
+
+from .utils         import ColorRGBA, \
+                           add_search_path, \
+                           res_pkg_path
+
+from .basic_simulator import BasicSimulator, \
+                             ContactPoint, \
+                             SimulatorPlugin
