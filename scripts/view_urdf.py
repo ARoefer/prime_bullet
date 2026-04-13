9b4af4833242a1f5e761a09ddc08a1b952b48962
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2026-04-13 11:48:41 +0200

    Updated URDF viewer

--- scripts/view_urdf.py
+++ scripts/view_urdf.py
@@ -3,7 +3,7 @@ import sys
 from time import time
 from math import sin
 
-from prime_bullet import Simulator
+import prime_bullet as pb
 
 
 if __name__ == '__main__':
@@ -12,11 +12,13 @@ if __name__ == '__main__':
         print('Name of urdf to load required. ROS package URIs are supported.')
     else:
         
-        sim = Simulator()
+        sim = pb.Simulator()
         sim.init(mode='gui')
         floor = sim.create_box(extents=[10,10,0.1], mass=0)
         thing = sim.load_urdf(sys.argv[1], useFixedBase=1)
-        box   = sim.create_box(extents=[1,1,1], mass=20, pos=[0,0,10])
+        box   = sim.create_box(extents=[1,1,1],
+                               mass=20,
+                               pose=pb.Transform.from_xyz(0,0,10))
 
         last_update = time()
         while True:
